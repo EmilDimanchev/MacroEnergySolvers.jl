5fa2f800053a8693a1cdcbe88bea45eebd7db404
Emil Dimanchev <emild@mit.edu>
2026-05-27 13:41:56 -0400

    flush

--- src/benders/algorithms.jl
+++ src/benders/algorithms.jl
@@ -162,6 +162,8 @@ function benders(planning_problem::Model,subproblems::Union{Vector{Dict{Any, Any
 			@info("$info_string")
 		end
 		flush(stdout)
+		flush(stderr)
+
         if running_gap <= ConvTol
 			if running_gap < 0
 				@info("*** Warning: Negative gap detected, terminating (Gap= $(round_from_tol(running_gap, ConvTol, 2)))  ***")
